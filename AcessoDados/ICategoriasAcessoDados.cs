aee72d0d264bf3237f1d8d02347f8c8af7d8ec00
JosueAOdeveza <104102636+JosueAOdeveza@users.noreply.github.com>
2022-04-21 18:11:37 -0300

    Programa de material de construção
    Programa de Material de Construção

---
+++ AcessoDados/ICategoriasAcessoDados.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace AcessoDados
+{
+    public interface ICategoriasAcessoDados
+    {
+        void Alterar(int idCategoria, string nome, string descricao);
+        bool Equals(object obj);
+        void Excluir(int idCategoria);
+        DataTable Listar();
+        void Salvar(string nome, string descricao);
+    }
+}
---
+++ MateriaisParaConstrucao/Microsoft/Reporting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Microsoft
+{
+    internal class Reporting
+    {
+        internal class WinForms
+        {
+            internal class ReportViewer
+            {
+                public DockStyle Dock { get; internal set; }
+                public object LocalReport { get; internal set; }
+                public string Name { get; internal set; }
+                public Size Size { get; internal set; }
+                public int TabIndex { get; internal set; }
+                public Point Location { get; internal set; }
+
+                internal void RefreshReport()
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            internal class ReportDataSource
+            {
+                public string Name { get; internal set; }
+                public BindingSource Value { get; internal set; }
+            }
+        }
+    }
+}
